c5153c21337c436b5534b769f0f33e7d8f15e0c6
Steffi Pereira <steffipereira23@gmail.com>
2021-01-24 18:56:12 +0000

    using hooks to fetch data from api and pull to refresh

--- screens/Home.js
+++ screens/Home.js
@@ -1,54 +1,36 @@
 import React from 'react';
 import { View, Text, StyleSheet, FlatList } from 'react-native';
 import ColorPreview from '../components/ColorPreview';
-
-const COLORS = [
-  { colorName: 'Base03', hexCode: '#002b36' },
-  { colorName: 'Base02', hexCode: '#073642' },
-  { colorName: 'Base01', hexCode: '#586e75' },
-  { colorName: 'Base00', hexCode: '#657b83' },
-  { colorName: 'Base0', hexCode: '#839496' },
-  { colorName: 'Base1', hexCode: '#93a1a1' },
-  { colorName: 'Base2', hexCode: '#eee8d5' },
-  { colorName: 'Base3', hexCode: '#fdf6e3' },
-  { colorName: 'Yellow', hexCode: '#b58900' },
-  { colorName: 'Orange', hexCode: '#cb4b16' },
-  { colorName: 'Red', hexCode: '#dc322f' },
-  { colorName: 'Magenta', hexCode: '#d33682' },
-  { colorName: 'Violet', hexCode: '#6c71c4' },
-  { colorName: 'Blue', hexCode: '#268bd2' },
-  { colorName: 'Cyan', hexCode: '#2aa198' },
-  { colorName: 'Green', hexCode: '#859900' },
-];
-
-const RAINBOW = [
-  { colorName: 'Red', hexCode: '#FF0000' },
-  { colorName: 'Orange', hexCode: '#FF7F00' },
-  { colorName: 'Yellow', hexCode: '#FFFF00' },
-  { colorName: 'Green', hexCode: '#00FF00' },
-  { colorName: 'Violet', hexCode: '#8B00FF' },
-];
-
-const RUBY = [
-  { colorName: 'Red', hexCode: '#c02d28' },
-  { colorName: 'Black', hexCode: '#3e3e3e' },
-  { colorName: 'Grey', hexCode: '#8a8a8a' },
-  { colorName: 'White', hexCode: '#ffffff' },
-  { colorName: 'Orange', hexCode: '#e66225' },
-];
-
-const THEMES = [
-  { paletteName: 'Theme main', colors: COLORS },
-  { paletteName: 'Theme ruby', colors: RUBY },
-  { paletteName: 'Theme rainbow', colors: RAINBOW },
-];
+import { useState, useCallback, useEffect } from 'react';
 
 const Home = ({ navigation }) => {
+  const [themes, setThemeNames] = useState([]);
+  const [refresh, setRefresh] = useState(false);
+  const URL = 'https://color-palette-api.kadikraman.now.sh/palettes';
+
+  const handleFetchThemes = useCallback(async () => {
+    const response = await fetch(URL);
+    if (response.ok) {
+      const themeList = await response.json();
+      setThemeNames(themeList);
+    }
+  }, []);
+
+  const handleScreenRefresh = useCallback(async () => {
+    setRefresh(true);
+    await handleFetchThemes;
+    setRefresh(false);
+  }, []);
+
+  useEffect(() => {
+    handleFetchThemes();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Here are some of my favorite themes</Text>
       <FlatList
-        data={THEMES}
+        data={themes.slice(3)}
         keyExtractor={(item) => item.paletteName}
         renderItem={({ item }) => (
           <ColorPreview
@@ -56,6 +38,8 @@ const Home = ({ navigation }) => {
             themeName={item}
           />
         )}
+        refreshing={refresh}
+        onRefresh={handleScreenRefresh}
       />
     </View>
   );
